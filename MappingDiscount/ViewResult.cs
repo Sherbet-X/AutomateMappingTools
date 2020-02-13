7b811077bed0fde4148c6ea9a6624dbce949ba4b
Sherbet-X <Mocca_Kluay@hotmail.com>
2020-02-13 14:43:19 +0700

    Revert "Enter"
    This reverts commit 4d4d46e77f5f0b475297a3b6b6a512f70e7b31d1.

--- MappingDiscount/ViewResult.cs
+++ MappingDiscount/ViewResult.cs
@@ -863,7 +863,6 @@ namespace MappingDiscount
             dataGridViewResult.Size = new Size(w, h - 280);
         }
 
-
         private void panel1_MouseDown(object sender, MouseEventArgs e)
         {
             mov = 1;
